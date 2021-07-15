a762036bbff3d5da7a7c1e65fa2024821b01198b
wilile26811249 <wilile26811249@gmail.com>
2021-07-15 15:23:22 +0800

    Update the improve spectral norm for discriminator

--- models.py
+++ models.py
@@ -12,8 +12,10 @@ class SLN(nn.Module):
     def __init__(self, num_features):
         super(SLN, self).__init__()
         self.ln = nn.LayerNorm(num_features)
-        self.gamma = nn.Parameter(torch.FloatTensor(1, 1, 1))
-        self.beta = nn.Parameter(torch.FloatTensor(1, 1, 1))
+        # self.gamma = nn.Parameter(torch.FloatTensor(1, 1, 1))
+        # self.beta = nn.Parameter(torch.FloatTensor(1, 1, 1))
+        self.gamma = nn.Parameter(torch.randn(1, 1, 1))
+        self.beta = nn.Parameter(torch.randn(1, 1, 1))
 
     def forward(self, hl, w):
         return self.gamma * w * self.ln(hl) + self.beta * w
@@ -64,8 +66,16 @@ class Attention(nn.Module):
         self.discriminator = discriminator
         self.w_out = nn.Linear(self.weight_dim, dim, bias = True)
 
+        if discriminator:
+            u, s, v = torch.svd(self.to_qkv.weight)
+            self.init_spect_norm = torch.max(s)
+
     def forward(self, x):
         assert x.dim() == 3
+
+        if self.discriminator:
+            u, s, v = torch.svd(self.to_qkv.weight)
+            self.to_qkv.weight = torch.nn.Parameter(self.to_qkv.weight * self.init_spect_norm / torch.max(s))
 
         # Generate the q, k, v vectors
         qkv = self.to_qkv(x)
@@ -247,6 +257,64 @@ class Generator(nn.Module):
         result = x.view(x.shape[0], 3, self.initialize_size * 8, self.initialize_size * 8)
         return result
 
+def l2normalize(v, eps=1e-12):
+    return v / (v.norm() + eps)
+
+class SpectralNorm(nn.Module):
+    def __init__(self, module, name='weight', power_iterations=1):
+        super(SpectralNorm, self).__init__()
+        self.module = module
+        self.name = name
+        self.power_iterations = power_iterations
+        if not self._made_params():
+            self._make_params()
+
+    def _update_u_v(self):
+        u = getattr(self.module, self.name + "_u")
+        v = getattr(self.module, self.name + "_v")
+        w = getattr(self.module, self.name + "_bar")
+
+        height = w.data.shape[0]
+        for _ in range(self.power_iterations):
+            v.data = l2normalize(torch.mv(torch.t(w.view(height,-1).data), u.data))
+            u.data = l2normalize(torch.mv(w.view(height,-1).data, v.data))
+
+        # sigma = torch.dot(u.data, torch.mv(w.view(height,-1).data, v.data))
+        sigma = u.dot(w.view(height, -1).mv(v))
+        setattr(self.module, self.name, w / sigma.expand_as(w))
+
+    def _made_params(self):
+        try:
+            u = getattr(self.module, self.name + "_u")
+            v = getattr(self.module, self.name + "_v")
+            w = getattr(self.module, self.name + "_bar")
+            return True
+        except AttributeError:
+            return False
+
+
+    def _make_params(self):
+        w = getattr(self.module, self.name)
+
+        height = w.data.shape[0]
+        width = w.view(height, -1).data.shape[1]
+
+        u = nn.Parameter(w.data.new(height).normal_(0, 1), requires_grad=False)
+        v = nn.Parameter(w.data.new(width).normal_(0, 1), requires_grad=False)
+        u.data = l2normalize(u.data)
+        v.data = l2normalize(v.data)
+        w_bar = nn.Parameter(w.data)
+
+        del self.module._parameters[self.name]
+
+        self.module.register_parameter(self.name + "_u", u)
+        self.module.register_parameter(self.name + "_v", v)
+        self.module.register_parameter(self.name + "_bar", w_bar)
+
+
+    def forward(self, *args):
+        self._update_u_v()
+        return self.module.forward(*args)
 
 class Discriminator(nn.Module):
     def __init__(self,
