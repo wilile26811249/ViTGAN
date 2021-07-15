c96bee9db1f9e5d966cdc2f376283bd7d6b211f4
wilile26811249 <wilile26811249@gmail.com>
2021-07-15 17:19:17 +0800

    Remove unsed part and fix some error about the SLN

--- models.py
+++ models.py
@@ -14,8 +14,8 @@ class SLN(nn.Module):
         self.ln = nn.LayerNorm(num_features)
         # self.gamma = nn.Parameter(torch.FloatTensor(1, 1, 1))
         # self.beta = nn.Parameter(torch.FloatTensor(1, 1, 1))
-        self.gamma = nn.Parameter(torch.randn(1, 1, 1))
-        self.beta = nn.Parameter(torch.randn(1, 1, 1))
+        self.gamma = nn.Parameter(torch.randn(1, 1, 1)) #.to("cuda")
+        self.beta = nn.Parameter(torch.randn(1, 1, 1)) #.to("cuda")
 
     def forward(self, hl, w):
         return self.gamma * w * self.ln(hl) + self.beta * w
@@ -243,78 +243,20 @@ class Generator(nn.Module):
         self.Transformer_Encoder = GTransformerEncoder(dim, blocks, num_heads, dim_head, dropout)
 
         # Implicit Neural Representation
-        # self.w_out = nn.Linear(dim, self.initialize_size * 8 * self.out_channels)
         self.w_out = nn.Sequential(
             SineLayer(dim, dim * 2, is_first = True, omega_0 = 30.),
             SineLayer(dim * 2, self.initialize_size * 8 * self.out_channels, is_first = False, omega_0 = 30)
         )
+        self.sln_norm = SLN(self.dim)
 
     def forward(self, noise):
         x = self.mlp(noise).view(-1, self.initialize_size * 8, self.dim)
         x, hl = self.Transformer_Encoder(self.pos_emb1D, x)
-        x = SLN(self.dim)(hl, x)
+        x = self.sln_norm(hl, x)
         x = self.w_out(x)  # Replace to siren
         result = x.view(x.shape[0], 3, self.initialize_size * 8, self.initialize_size * 8)
         return result
 
-def l2normalize(v, eps=1e-12):
-    return v / (v.norm() + eps)
-
-class SpectralNorm(nn.Module):
-    def __init__(self, module, name='weight', power_iterations=1):
-        super(SpectralNorm, self).__init__()
-        self.module = module
-        self.name = name
-        self.power_iterations = power_iterations
-        if not self._made_params():
-            self._make_params()
-
-    def _update_u_v(self):
-        u = getattr(self.module, self.name + "_u")
-        v = getattr(self.module, self.name + "_v")
-        w = getattr(self.module, self.name + "_bar")
-
-        height = w.data.shape[0]
-        for _ in range(self.power_iterations):
-            v.data = l2normalize(torch.mv(torch.t(w.view(height,-1).data), u.data))
-            u.data = l2normalize(torch.mv(w.view(height,-1).data, v.data))
-
-        # sigma = torch.dot(u.data, torch.mv(w.view(height,-1).data, v.data))
-        sigma = u.dot(w.view(height, -1).mv(v))
-        setattr(self.module, self.name, w / sigma.expand_as(w))
-
-    def _made_params(self):
-        try:
-            u = getattr(self.module, self.name + "_u")
-            v = getattr(self.module, self.name + "_v")
-            w = getattr(self.module, self.name + "_bar")
-            return True
-        except AttributeError:
-            return False
-
-
-    def _make_params(self):
-        w = getattr(self.module, self.name)
-
-        height = w.data.shape[0]
-        width = w.view(height, -1).data.shape[1]
-
-        u = nn.Parameter(w.data.new(height).normal_(0, 1), requires_grad=False)
-        v = nn.Parameter(w.data.new(width).normal_(0, 1), requires_grad=False)
-        u.data = l2normalize(u.data)
-        v.data = l2normalize(v.data)
-        w_bar = nn.Parameter(w.data)
-
-        del self.module._parameters[self.name]
-
-        self.module.register_parameter(self.name + "_u", u)
-        self.module.register_parameter(self.name + "_v", v)
-        self.module.register_parameter(self.name + "_bar", w_bar)
-
-
-    def forward(self, *args):
-        self._update_u_v()
-        return self.module.forward(*args)
 
 class Discriminator(nn.Module):
     def __init__(self,
